3d6901c1fbb25cd61753981a95ccb9d0ecb1f595
faizaanshamsi <faizaanshamsi@gmail.com>
2014-01-06 08:30:37 -0500

    initial config complete

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,14 @@
-# Be sure to restart your server when you modify this file.
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
 
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-CarListing::Application.config.secret_key_base = '5a23d75d0747747c0b0a51b8c655033dab88e183ee159fe29831669606b6c6bf680f8f64ff14a9a4d831dff89132d8dd17ef352dcf5f90e82082884391f2aedc'
+CarListing::Application.config.secret_key_base = secure_token
